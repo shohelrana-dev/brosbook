9c37ab466ada02febb2c8b870f285f7eeb3cb5f8
Shohel Rana <shohelrrrana@gmail.com>
2023-01-19 01:37:36 +0600

    optimize seo

--- src/app/[username]/head.tsx
+++ src/app/[username]/head.tsx
@@ -16,10 +16,19 @@ export default async function Head( { params }: Props ){
     const config     = authorizationConfig( cookies() )
     const user: User = await http.get( `/users/by/username/${ params.username }`, config ).then( ( res ) => res.data ).catch( () => null )
 
+    const title       = `${ user.fullName } (@${ user.username }) | ${ process.env.NEXT_PUBLIC_APP_NAME }`
+    const description = user.profile?.bio
+
     return (
         <>
             <DefaultTags/>
-            <title>{ `${user.fullName} (@${ user.username }) | ${ process.env.NEXT_PUBLIC_APP_NAME }` }</title>
+            <meta content="profile" property="og:type"/>
+            <meta content={ description } property="og:description"/>
+            {/*@ts-ignore*/}
+            <meta description={ description } name="description"/>
+            <meta content={ user.avatar.url } property="og:image"/>
+            <meta content={ title } property="og:title"/>
+            <title>{ title }</title>
         </>
     )
 }
--- src/components/common/DefaultTags.tsx
+++ src/components/common/DefaultTags.tsx
@@ -7,9 +7,10 @@ function DefaultTags(){
             <meta name="viewport"
                   content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
             <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
-            <meta property="og:site_name" content="brosbook"/>
+            <meta name="color-scheme" content="light"/>
+            <meta name="theme-color" content="#FFFFFF"/>
+            <meta property="og:site_name" content={process.env.NEXT_PUBLIC_APP_NAME}/>
             <meta property="twitter:card" content="summary"/>
-            <meta property="og:type" content="website"/>
             <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon.png"/>
         </>
     )
