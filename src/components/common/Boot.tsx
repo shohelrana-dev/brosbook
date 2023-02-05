e7b2c3dc63d0e81d46679b4be69fdd1fb46a518f
Shohel Rana <shohelrrrana@gmail.com>
2023-02-06 00:01:00 +0600

    update user activity via socket io

--- src/components/common/Boot.tsx
+++ src/components/common/Boot.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch } from "react-redux"
 import { useGetCurrentUserQuery } from "@services/usersApi"
 import { setAuth } from "@slices/authSlice"
+import { io } from "socket.io-client";
 
 export default function Boot(){
     const dispatch       = useDispatch()
@@ -10,6 +11,22 @@ export default function Boot(){
     useEffect( () => {
         if( user && Object.keys( user! ).length > 0 ){
             dispatch( setAuth( user! ) )
+        }
+    }, [user] )
+
+    useEffect( () => {
+        if( ! user || Object.keys( user ).length < 1 ) return
+
+        const socket = io( process.env.NEXT_PUBLIC_SERVER_BASE_URL! )
+
+        if( user && Object.keys( user ).length > 0 ){
+            socket.on( "connect", () => {
+                socket.emit( 'connect_user', user )
+            } )
+        }
+
+        if( socket ) return () => {
+            socket.close()
         }
     }, [user] )
 
