5426909c6b439e57063d453d9275c36483185246
Shohel Rana <shohelrrrana@gmail.com>
2023-02-09 00:15:25 +0600

    fix tab link

--- src/components/common/TabLinkList.tsx
+++ src/components/common/TabLinkList.tsx
@@ -1,29 +1,29 @@
 "use client"
 import React from 'react'
-import {usePathname} from "next/navigation"
+import { usePathname } from "next/navigation"
 import Link from "next/link"
 import classNames from "classnames"
 
-interface TabLinkListProps{
-    links: {label: string, pathname: string}[]
+interface TabLinkListProps {
+    links: { label: string, pathname: string }[]
 }
 
-function TabLinkList(props: TabLinkListProps) {
+function TabLinkList( props: TabLinkListProps ){
     const pathname = usePathname()
 
-    const className = 'inline-block py-2 px-3 sm:px-4 text-gray-600 rounded mr-4 border-0 border-b-4 border-transparent hover:text-theme-blue'
+    const className = 'inline-block py-2 px-3 sm:px-4 rounded mr-4 border-0 border-b-4 hover:text-theme-blue'
 
     return (
         <div className="mt-5">
-            {props.links.map(link => (
+            { props.links.map( link => (
                 <Link
-                    key={link.pathname}
-                    href={link.pathname}
-                    className={classNames(className, pathname === link.pathname ? 'border-theme-blue text-theme-blue' : '')}
+                    key={ link.pathname }
+                    href={ link.pathname }
+                    className={ classNames( className, pathname === link.pathname ? 'border-theme-blue text-theme-blue' : 'text-gray-600 border-transparent' ) }
                 >
-                    {link.label}
+                    { link.label }
                 </Link>
-            ))}
+            ) ) }
             <hr/>
         </div>
     )
