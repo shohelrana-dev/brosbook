32e77b89b294e95ac05be08526d0cb90d8004951
Shohel Rana <shohelrrrana@gmail.com>
2023-02-09 14:09:07 +0600

    fix avatar

--- src/components/common/Avatar.tsx
+++ src/components/common/Avatar.tsx
@@ -19,7 +19,7 @@ function Avatar( { online, size, src, className }: AvatarProps ){
         height = 60
     }
 
-    className = classNames( `rounded-full`, className )
+    className = classNames( `rounded-full absolute left-0 top-0`, className )
 
     return (
         <div className="relative flex-none rounded-full" style={ { width, height } }>
@@ -27,9 +27,10 @@ function Avatar( { online, size, src, className }: AvatarProps ){
                 priority
                 className={ className }
                 src={ src }
-                fill={true}
-                alt="Profile photo"
-                sizes={`${width}px ${height}px`}
+                width={ width }
+                height={ height }
+                style={{width: `${width}px`, height: `${height}px`}}
+                alt="User profile photo"
             /> : null }
             { online ? <div
                 className="bg-green-500 border-white w-3 h-3 absolute right-0 bottom-0 rounded-full border-2"/> : null }
