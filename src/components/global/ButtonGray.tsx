53efb2a631d672dccddf711f43587d9c4260e4cf
Shohel Rana <shohelrrrana@gmail.com>
2023-02-17 23:37:40 +0600

    fixes

--- src/components/global/ButtonGray.tsx
+++ src/components/global/ButtonGray.tsx
@@ -9,7 +9,7 @@ export interface ButtonGrayProps extends BaseButtonProps {
 
 export default function ButtonGray( props: ButtonGrayProps ){
     let { children, isLoading = false, className, disabled, size = 'sm', ...rest } = props
-    className                                                                      = classNames( 'mt-3 rounded-full capitalize min-w-[90px] bg-[#dddddd38] hover:bg-[#dddddd38]', className )
+    className                                                                      = classNames( 'mt-3 rounded-full capitalize min-w-[90px] bg-[#dddddd14] hover:bg-[#dddddd38]', className )
 
     if( isLoading ) return (
         <div className="ml-6 py-4">
