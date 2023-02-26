6b4d4737b03d31d1a03ee9e56c5795da40704190
Shohel Rana <shohelrrrana@gmail.com>
2023-02-27 00:48:54 +0600

    change error ui

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,11 +1,18 @@
 "use client"
 import Button from "@components/global/Button"
+import { IoReload as ReloadIcon } from "react-icons/io5"
+import { BiErrorCircle as ErrorIcon } from "react-icons/bi"
 
 export default function Error( { error, reset }: { error: Error; reset: () => void; } ){
     return (
         <div className="w-full h-full flex items-center justify-center flex-col py-5">
-            <h2 className="text-lg font-bold">Something went wrong!</h2>
-            <Button onClick={ reset }>Try again</Button>
+            <div className="mb-5">
+                <ErrorIcon size={40} className="text-gray-700"/>
+            </div>
+            <h2 className="text-lg font-bold text-gray-900 mb-1">Something went wrong. Try reloading.</h2>
+            <Button onClick={ reset } size="md" className="flex">
+                <ReloadIcon size={ 20 }/>&nbsp;Try again
+            </Button>
         </div>
     )
 }
