bdc84446438665e2a533fd6e60eb46003adf4eae
Shohel Rana <shohelrrrana@gmail.com>
2023-03-01 01:33:36 +0600

    fixes

--- src/components/widgets/NewUserMessage.tsx
+++ src/components/widgets/NewUserMessage.tsx
@@ -2,20 +2,27 @@
 import React from 'react'
 import Link from "next/link"
 import ButtonGray from "@components/global/ButtonGray"
+import useAuthState from "@hooks/useAuthState"
 
-function NewUserMessage(  ){
-    return (
-        <div className="box p-5">
-            <h2 className="text-xl font-medium mb-2">New to { process.env.NEXT_PUBLIC_APP_NAME }?</h2>
-            <p className="text-gray-800">Sign up now to get your own personalized timeline!</p>
+function NewUserMessage(){
+    const { isChecked, isAuthenticated } = useAuthState()
 
-            <Link href="/auth/signup">
-                <ButtonGray>
-                    Goto Signup page
-                </ButtonGray>
-            </Link>
-        </div>
-    )
+    if( isChecked && ! isAuthenticated ){
+        return (
+            <div className="box p-5">
+                <h2 className="text-xl font-medium mb-2">New to { process.env.NEXT_PUBLIC_APP_NAME }?</h2>
+                <p className="text-gray-800">Sign up now to get your own personalized timeline!</p>
+
+                <Link href="/auth/signup">
+                    <ButtonGray>
+                        Goto Signup page
+                    </ButtonGray>
+                </Link>
+            </div>
+        )
+    }
+
+    return null
 }
 
 export default NewUserMessage
