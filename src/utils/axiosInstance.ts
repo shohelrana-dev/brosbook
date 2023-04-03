325d6770171099168eb4fbead7a796c291f7da33
Shohel Rana <shohelrrrana@gmail.com>
2023-04-03 18:57:20 +0600

    axios timeout

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -5,5 +5,6 @@ const access_token = typeof window !== 'undefined' ? getCookie( 'access_token' )
 
 export const http = axios.create( {
     baseURL: process.env.NEXT_PUBLIC_SERVER_API_URL,
-    headers: { Authorization: `Bearer ${ access_token }` }
+    headers: { Authorization: `Bearer ${ access_token }` },
+    timeout: 30000
 } )
