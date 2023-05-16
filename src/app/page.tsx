d4982081ce0e3fa65182a36c27375ea78be339bd
Shohel Rana <shohelrrrana@gmail.com>
2023-05-17 00:06:53 +0600

    update home seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,11 @@ import FeedPosts from "./FeedPosts"
 import SidebarLayout from "@components/global/SidebarLayout"
 import { Metadata } from "next"
 import CreatePostForm from "@components/post/CreatePostForm"
-import SlidesSuggestions from "@components/global/SlidesSuggestions";
+import SlidesSuggestions from "@components/global/SlidesSuggestions"
 
 export const metadata: Metadata = {
-    title: `Home | ${ process.env.NEXT_PUBLIC_APP_NAME }`
+    title: `Home | ${ process.env.NEXT_PUBLIC_APP_NAME }`,
+    description: 'A social media platform developed by Shohel Rana'
 }
 
 export default function Page(){
