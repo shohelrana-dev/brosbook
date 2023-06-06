6a46cfa8c55ba209ed9b0ddb63c2f3f0e50dc149
Shohel Rana <shohelrrrana@gmail.com>
2023-06-06 15:34:00 +0600

    fix loader

--- src/app/[username]/media/page.tsx
+++ src/app/[username]/media/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { useGetMediaListQuery, useGetUserByUsernameQuery } from "@services/usersApi"
 import Loader from "@components/global/Loader"
 import ImageLightbox from "@components/global/ImageLightbox"
@@ -11,10 +11,10 @@ interface Props {
     params: { username: string }
 }
 
-export default function MediaPage( { params }: Props ){
-    const [page, setPage] = useState( 1 )
-    const { data: user }  = useGetUserByUsernameQuery( params.username )
-    const mediaListQuery  = useGetMediaListQuery( { userId: user?.id!, page }, { skip: ! user?.id } )
+export default function MediaPage( { params }: Props ) {
+    const [ page, setPage ] = useState(1)
+    const { data: user }    = useGetUserByUsernameQuery(params.username)
+    const mediaListQuery    = useGetMediaListQuery({ userId: user?.id!, page }, { skip: !user?.id })
 
     const { data: mediaListData, isLoading, isSuccess, isError } = mediaListQuery || {}
     const { items: mediaList = [], nextPage }                    = mediaListData || {}
@@ -22,18 +22,18 @@ export default function MediaPage( { params }: Props ){
 
     //decide content
     let content = null
-    if( isLoading ){
+    if ( isLoading ) {
         content = <div className="py-3"><Loader size={ 50 }/></div>
-    } else if( isSuccess && mediaList.length === 0 ){
+    } else if ( isSuccess && mediaList.length === 0 ) {
         content = <p className="text-center py-3">{ user?.fullName } haven't media.</p>
-    } else if( isError ){
+    } else if ( isError ) {
         content = <Error message={ error?.data?.message }/>
-    } else if( isSuccess && mediaList.length > 0 ){
+    } else if ( isSuccess && mediaList.length > 0 ) {
         content = (
             <InfiniteScroll
                 dataLength={ mediaList.length }
-                next={ () => setPage( nextPage! ) }
-                hasMore={ !! nextPage }
+                next={ () => setPage(nextPage!) }
+                hasMore={ !!nextPage }
                 loader={ <Loader size={ 40 }/> }
             >
                 <ImageLightbox imageList={ mediaList } width={ 200 } height={ 200 } alt="Media"/>
--- src/components/global/Loader.tsx
+++ src/components/global/Loader.tsx
@@ -1,3 +1,4 @@
+"use client"
 import CircularProgress, {
     circularProgressClasses,
     CircularProgressProps,
