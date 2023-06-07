597509d57963ebb9a18fb502e70f23a415dfc47f
Shohel Rana <shohelrrrana@gmail.com>
2023-06-07 16:02:36 +0600

    fix issue

--- src/components/messages/ParticipantInfo.tsx
+++ src/components/messages/ParticipantInfo.tsx
@@ -1,67 +1,67 @@
 import Avatar from "@components/global/Avatar"
 import { useGetConversationByIdQuery, useGetConversationMediaListQuery } from "@services/conversationsApi"
 import Loader from "@components/global/Loader"
-import {ErrorResponse} from "@interfaces/index.interfaces"
+import { ErrorResponse } from "@interfaces/index.interfaces"
 import ImageLightbox from "@components/global/ImageLightbox"
-import React, {useEffect, useRef, useState} from "react"
+import React, { useEffect, useRef, useState } from "react"
 import tw, { styled } from "twin.macro"
 import { Box as BaseBox } from "@components/styles/Global.styles"
-import {useParams} from "next/navigation"
+import { useParams } from "next/navigation"
 import Error from "@components/global/Error"
 import useInfiniteScroll from "react-infinite-scroll-hook"
 
 const Wrapper  = tw.div`h-full overflow-y-auto scrollbar-hide`
-const Box      = tw( BaseBox )`p-5 mb-3`
+const Box      = tw(BaseBox)`p-5 mb-3`
 const Header   = tw.header`flex flex-col justify-center items-center`
 const Name     = tw.h3`font-medium text-lg text-gray-800 mt-3`
 const Bio      = tw.p`text-gray-600`
-const Label    = tw.p`text-gray-600 mb-0.5`
-const Heading  = tw.h3`text-base font-medium mb-3`
-const InfoItem = styled.div( ( { last }: { last?: boolean } ) => [
+const Label    = tw.p`text-gray-900 font-medium mb-0.5`
+const Heading  = tw.h3`text-lg font-medium mb-3`
+const InfoItem = styled.div(( { last }: { last?: boolean } ) => [
     tw`pb-2 mb-2`,
-    ! last && tw`border-0 border-b-[1px] border-solid border-gray-200`
-] )
+    !last && tw`border-0 border-b-[1px] border-solid border-gray-200`
+])
 
-export default function ParticipantInfo(  ){
+export default function ParticipantInfo() {
     //hooks
-    const {conversationId} = useParams()
-    const { data: conversation } = useGetConversationByIdQuery( conversationId )
-    const [page, setPage] = useState<number>( 1 )
-    const conversationMediaListQuery  = useGetConversationMediaListQuery({conversationId, page} )
+    const { conversationId }                                = useParams()
+    const { data: conversation }                            = useGetConversationByIdQuery(conversationId)
+    const [ page, setPage ]                                 = useState<number>(1)
+    const conversationMediaListQuery                        = useGetConversationMediaListQuery({ conversationId, page })
     const { isLoading, isSuccess, isError, data: listData } = conversationMediaListQuery || {}
-    const containerRef                                                       = useRef<HTMLDivElement>( null )
+    const containerRef                                      = useRef<HTMLDivElement>(null)
 
-    const { items: mediaList = [], nextPage }                    = listData || {}
-    const error                                              = conversationMediaListQuery.error as ErrorResponse || {}
+    const { items: mediaList = [], nextPage } = listData || {}
+    const error                               = conversationMediaListQuery.error as ErrorResponse || {}
 
-    useEffect( () => {
-        if( containerRef.current && containerRef.current.parentElement ){
+    useEffect(() => {
+        if ( containerRef.current && containerRef.current.parentElement ) {
             containerRef.current.style.height = `${ containerRef?.current?.parentElement?.offsetHeight }px`
         }
-    }, [] )
+    }, [])
 
-    const [moreLoadRef] = useInfiniteScroll( {
+    const [ moreLoadRef ] = useInfiniteScroll({
         loading: isLoading,
-        hasNextPage: !! nextPage,
-        onLoadMore: () => setPage( nextPage! )
-    } )
+        hasNextPage: !!nextPage,
+        onLoadMore: () => setPage(nextPage!)
+    })
 
     const participant = conversation?.participant
 
-    if( ! participant ) return null
+    if ( !participant ) return null
 
     const { fullName, avatar, email, active } = participant
     const { bio, phone, location }            = participant.profile || {}
 
     //decide listContent
     let listContent = null
-    if( isLoading ){
+    if ( isLoading ) {
         listContent = <Loader/>
-    } else if( isSuccess && mediaList.length === 0 ){
+    } else if ( isSuccess && mediaList.length === 0 ) {
         listContent = <p className="text-center py-4">No media files.</p>
-    } else if( isError ){
+    } else if ( isError ) {
         listContent = <Error message={ error.data?.message }/>
-    } else if( isSuccess && mediaList.length > 0 ){
+    } else if ( isSuccess && mediaList.length > 0 ) {
         listContent = <ImageLightbox imageList={ mediaList } alt="Chat photo"/>
     }
 
@@ -70,7 +70,7 @@ export default function ParticipantInfo(  ){
             <Box>
                 <Header>
                     <Avatar
-                        online={ !! active }
+                        online={ !!active }
                         src={ avatar.url }
                         size="large"
                         alt={ fullName }
@@ -82,18 +82,20 @@ export default function ParticipantInfo(  ){
 
             <Box>
                 <Heading>Personal Information</Heading>
-                <InfoItem>
+                { location ? <InfoItem>
                     <Label>Address</Label>
                     <div>{ location }</div>
-                </InfoItem>
-                <InfoItem>
+                </InfoItem> : null }
+
+                { phone ? <InfoItem>
                     <Label>Phone</Label>
                     <div>{ phone }</div>
-                </InfoItem>
-                <InfoItem last>
+                </InfoItem> : null }
+
+                { email ? <InfoItem last>
                     <Label>Email</Label>
                     <div>{ email }</div>
-                </InfoItem>
+                </InfoItem> : null }
             </Box>
 
             <Box>
@@ -102,7 +104,7 @@ export default function ParticipantInfo(  ){
                 <div>{ listContent }</div>
 
                 { nextPage ? (
-                    <div className="py-4" ref={ moreLoadRef }>
+                    <div className="my-4" ref={ moreLoadRef }>
                         <Loader/>
                     </div>
                 ) : null }
