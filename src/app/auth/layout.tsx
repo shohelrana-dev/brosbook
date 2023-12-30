f3f98c9743ddd29cd9934e64d0ef43cebe536128
Shohel Rana <shohelrrrana@gmail.com>
2023-12-30 15:58:34 +0600

    page layout markup refactor

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,11 +1,9 @@
 import { PropsWithChildren } from 'react'
 
-export default function Layout( { children }: PropsWithChildren ){
+export default function Layout({ children }: PropsWithChildren) {
     return (
-        <div className="h-screen flex flex-wrap flex-col bg-theme-gray">
-            <div className="w-100 max-w-full mx-auto mt-12 lg:mt-28 relative z-50">
-                { children }
-            </div>
+        <div className='w-100 max-w-full mx-auto mt-12 lg:mt-28 relative z-50'>
+            {children}
         </div>
     )
 }
