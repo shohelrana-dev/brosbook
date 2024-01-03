63884cb1d718e804f573d9a5cf22d10ca17769b2
Shohel Rana <shohelrrrana@gmail.com>
2024-01-03 21:33:56 +0600

    remove getBlurImageData.ts

--- src/utils/getBlurImageData.ts
+++
@@ -1,8 +0,0 @@
-import plaiceholder from 'plaiceholder'
-export default async function getBlurImageData(url: string) {
-    return await plaiceholder.getPlaiceholder(url, {
-        width: 10,
-        height: 10,
-        quality: 10,
-    })
-}
