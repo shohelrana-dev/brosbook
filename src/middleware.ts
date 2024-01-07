a2e62f82f493c389c1f34994ef0f35fb792614ef
Shohel Rana <shohelrrrana@gmail.com>
2024-01-07 19:43:05 +0600

    refactor middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,52 +1,62 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import isAuthenticated from "@utils/isAuthenticated"
+import isAuthenticated from '@utils/isAuthenticated'
 
-export async function middleware( request: NextRequest ) {
-    const currentPathname  = request.nextUrl.pathname
-    const isUserAuthorized = await isAuthenticated( request.cookies )
-    const protectedPaths   = ['/messages', '/notifications', '/suggestions', '/account', '/account/profile', '/auth/logout']
-    const guestPaths       = ['/auth/login', '/auth/signup', '/auth/email_verification']
+const protectedPaths = [
+	'/messages',
+	'/notifications',
+	'/suggestions',
+	'/account',
+	'/account/profile',
+	'/auth/logout',
+]
+const guestPaths = ['/auth/login', '/auth/signup', '/auth/email_verification']
 
-    const isProtectedPath = Boolean( protectedPaths.find( path => currentPathname.startsWith( path ) )?.length )
-    const isGuestPath     = Boolean( guestPaths.find( path => currentPathname.startsWith( path ) )?.length )
-    const isHomePath      = currentPathname === '/'
-    
-    if ( !isGuestPath && !isProtectedPath && isHomePath ) {
-        return NextResponse.next()
-    }
+export async function middleware(request: NextRequest) {
+	const currentPathname = request.nextUrl.pathname
+	const isUserAuthorized = await isAuthenticated(request.cookies)
 
-    if ( isUserAuthorized ) {
-        if ( isGuestPath ) {
-            return NextResponse.redirect( new URL( '/', request.url ) )
-        }
-    } else {
-        if ( isProtectedPath && request.cookies.has( 'access_token' ) ) {
-            const response = NextResponse.redirect( new URL( `/auth/login?redirect_to=${ currentPathname }`, request.url ) )
-            response.cookies.delete( 'access_token' )
-        } else if ( isProtectedPath ) {
-            return NextResponse.redirect( new URL( `/auth/login?redirect_to=${ currentPathname }`, request.url ) )
-        }
-    }
+	const isProtectedPath = Boolean(
+		protectedPaths.find(path => path !== '/' && currentPathname.startsWith(path))?.length
+	)
+	const isGuestPath = Boolean(guestPaths.find(path => currentPathname.startsWith(path))?.length)
 
-    return NextResponse.next()
+	const shouldRedirect = (isProtectedPath && !isUserAuthorized) || (isGuestPath && isUserAuthorized)
+
+	// Conditionally delete access_token cookie for clarity
+	if (!isUserAuthorized && request.cookies.has('access_token')) {
+		request.cookies.delete('access_token')
+	}
+
+	if (shouldRedirect) {
+		if (isProtectedPath && !isUserAuthorized) {
+			return NextResponse.redirect(
+				new URL(`/auth/login?redirect_to=${currentPathname}`, request.url)
+			)
+		}
+
+		//authorized but guest path
+		return NextResponse.redirect(new URL('/', request.url))
+	}
+
+	return NextResponse.next()
 }
 
 export const config = {
-    matcher: [
-      /*
-       * Match all request paths except for the ones starting with:
-       * - api (API routes)
-       * - _next/static (static files)
-       * - _next/image (image optimization files)
-       * - favicon.ico (favicon file)
-       */
-      {
-        source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
-        missing: [
-          { type: 'header', key: 'next-router-prefetch' },
-          { type: 'header', key: 'purpose', value: 'prefetch' },
-        ],
-      },
-    ],
-  }
+	matcher: [
+		/*
+		 * Match all request paths except for the ones starting with:
+		 * - api (API routes)
+		 * - _next/static (static files)
+		 * - _next/image (image optimization files)
+		 * - favicon.ico (favicon file)
+		 */
+		{
+			source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+			missing: [
+				{ type: 'header', key: 'next-router-prefetch' },
+				{ type: 'header', key: 'purpose', value: 'prefetch' },
+			],
+		},
+	],
+}
